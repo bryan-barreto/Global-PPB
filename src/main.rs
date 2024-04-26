9b3842af05a9d4d763e7e3f18d0b9758fa6bf86a
bryan-barreto <bryan94barreto@gmail.com>
2024-04-25 22:20:50 -0400

    bi done

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,24 @@ use axum::{
 };
 use axum::response::Html;
 use handlebars::Handlebars;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::PgPool;
 use std::process::Command;
 use std::str;
+use tower_http::services::ServeDir;
 
 mod prelude;
 mod score_populate;
 
-
+#[derive(Serialize, Deserialize, Debug)]
+struct Graph{
+    filename: String,
+    title: String
+}
 
 #[tokio::main]
+
 async fn main() {
     dotenv::dotenv().unwrap();
     let db = PgPool::connect_lazy(&env::var("DATABASE_URL").expect("Database environment variable missing"))
@@ -30,7 +37,12 @@ async fn main() {
         .route("/", get(handler))
         .route("/playerdash", get(player_handler))
         .route("/businessdash", get(business_handler))
+        .nest_service(
+            "/images",
+            ServeDir::new("./images")
+        )
         .layer(Extension(db));
+    
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     
 
@@ -64,11 +76,13 @@ async fn player_handler() -> Html<String>{
 async fn business_handler() -> Html<String>{
     let chart_list = vec!["daily_uploads_chart", "average_player_improvement", "total_players", "favorite_stage_graph", "players_by_country"];
 
+    
 
-    let html_site = include_str!("../index.html");
+    let html_site = include_str!("../bi.html");
     let reg = Handlebars::new();
     let out_string= "Business Intelligence Dashboard".to_string();
-    let html_out = reg.render_template(html_site, &json!({"name": out_string})).expect("Error");
+    let mut graph_vec: Vec<Graph> = vec![]; 
+
     for chart in chart_list{
         let command =  "./src/graphs.py";
         let output = Command::new("python")
@@ -77,16 +91,27 @@ async fn business_handler() -> Html<String>{
             .expect("Failed to get {chart}")
             .stdout
         ;
-        let out_vector = str::from_utf8(&output)
+        let mut out_vector = str::from_utf8(&output)
             .unwrap()
-            .to_string()
-            .split("")
+            .lines()
         ;
+        
+        
+        while let (Some(filename), Some(title)) = (out_vector.next(), out_vector.next()){
+            graph_vec.push(Graph{filename:filename.to_string(),title:title.to_string()});
+        }
+
+
 
         // println!("{}", out_to_string);
         // html_out += 
     }
 
+    let out_json = &json!({"page_title": out_string,
+                            "graph": graph_vec
+                            });
+
+    let html_out = reg.render_template(html_site, out_json).expect("Error");
     Html(html_out)
 }
 
